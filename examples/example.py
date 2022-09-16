f31ba067dd66e1c04b5ec07f89e901ffab68f773
Yevgnen Koh <wherejoystarts@gmail.com>
2022-09-16 10:13:14 +0800

    Update example

--- examples/example.py
+++ examples/example.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
 
-# See and run examples/example.py
-
 import filefox
 
-expected = {
+obj = {
     "name": "John",
     "age": 12,
 }
 
-assert expected == filefox.read_json("json_data.json")
-assert expected == filefox.read_json("json_data.json.gz")
-assert expected == filefox.read("json_data.json")
-assert expected == filefox.read("json_data.json.gz")
+filefox.write_json(obj, "json_data.json")
+assert obj == filefox.read_json("json_data.json")
+
+filefox.write_json(obj, "json_data.json.gz")
+assert obj == filefox.read_json("json_data.json.gz")
+
+filefox.write(obj, "json_data.json")
+assert obj == filefox.read("json_data.json")
+
+filefox.write(obj, "json_data.json.gz")
+assert obj == filefox.read("json_data.json.gz")
+
+filefox.write_json(
+    obj, "json_data.json", file_kwargs={"mode": "w", "encoding": "utf8"}, indent=2
+)
+assert obj == filefox.read_json("json_data.json", file_kwargs={"encoding": "utf8"})
